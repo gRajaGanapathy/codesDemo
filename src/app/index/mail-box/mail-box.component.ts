696decdba0f615424cc8a5db7acbbb09e3381f44
raja <raja@twilightsoftwares.com>
2022-03-08 19:27:28 +0530

    mail box sent

--- src/app/index/mail-box/mail-box.component.ts
+++ src/app/index/mail-box/mail-box.component.ts
@@ -13,6 +13,22 @@ export class MailBoxComponent implements OnInit {
   declined: any;
   pending: any;
   tabType: any = 'regular';
+  readStatus = [
+    {name: "READ", checked: false, id: 1},
+    {name: "UNREAD", checked: false, id: 2}
+  ]
+  conversationType = [
+    {name: "ALL", checked: false, id: 1},
+    {name: "INTERESTS", checked: false, id: 2},
+    {name: "MESSAGES", checked: false, id: 3},
+    {name: "PHOTO REQUESTS", checked: false, id: 4},
+    {name: "HOROSCOPE REQUESTS", checked: false, id: 5},
+    {name: "PHONE NUMBER VIEWED", checked: false, id: 6},
+    {name: "OTHER REQUESTS", checked: false, id: 7},
+  ]
+  seletedValue = 'inbox';
+  public sentValue: string = "all";
+  dropdownlist: any;
 
 
   constructor(
@@ -26,13 +42,15 @@ export class MailBoxComponent implements OnInit {
     this.tabType = type;
   }
 
-  segmentChanged(ev: any) {
+  inboxChange(ev: any) {
     this.segment = ev.detail.value;
+  }
+  sentChange(e: any) {
+    this.sentValue = e.detail.value
   }
   getProfileList() {
     this.dataService.getProfileInfo().subscribe((result: any) => {
-      this.declined = result.data.declined;
-      this.pending = result.data.pending;
+      this.dropdownlist = result.data.dropdownlist;
       console.log('result in email', result.data)
     }, err => {
       console.log('getProfileInfo', err);
@@ -46,6 +64,10 @@ export class MailBoxComponent implements OnInit {
         type: type,
       }
     });
+  }
+  onSelect(e){
+    console.log(e.detail.value);
+    this.seletedValue = e.detail.value;
   }
 
 }
